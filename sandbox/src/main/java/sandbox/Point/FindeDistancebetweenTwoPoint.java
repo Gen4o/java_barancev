6a82e7613c6ed27e18b63127b9f950aaf320fdc6
Gennadii Miroshnychenko <genamister@gmail.com>
2016-05-24 02:08:14 +0300

    +1 example

--- sandbox/src/main/java/sandbox/Point/FindeDistancebetweenTwoPoint.java
+++ sandbox/src/main/java/sandbox/Point/FindeDistancebetweenTwoPoint.java
@@ -5,10 +5,13 @@ package sandbox.Point;
  */
 public class FindeDistancebetweenTwoPoint {
     public static void main(String[] args){
-        Point1 p1 = new Point1(7,7);
-        Point1 p2 = new Point1(4,4);
+        Point p1 = new Point(3,3);
+        Point p2 = new Point(2,1);
+        Point p3 = new Point(7,7);
+        Point p4 = new Point(6,6);
 
-        System.out.println("Расстояние между двумя точками c  координатами " + "p1 = ("+p1.x1+","+ p1.y1 +")"+ "p2 = ("+p2.x1+","+ p2.y1+")"+" = " +p1.distance(p1, p2));
+        System.out.println("Расстояние между двумя точками c  координатами " + "p1 = ("+p1.x1+","+ p1.y1 +")"+ " и p2 = ("+p2.x1+","+ p2.y1+")"+" = " +p1.distance(p1, p2));
+        System.out.println("Расстояние между двумя точками c  координатами " + "p3 = ("+p3.x1+","+ p3.y1 +")"+ " и p4 = ("+p4.x1+","+ p4.y1+")"+" = " +p1.distance(p3, p4));
 
     }
 }
