591215adcfb3bc491f155fa995f71d5d6a584ea0
Gennadii Miroshnychenko <genamister@gmail.com>
2016-05-23 23:51:21 +0300

    Созданы класс Rectangle & Square

---
+++ sandbox/src/main/java/stqa/barancev/sandbox/Rectangle.java
@@ -0,0 +1,14 @@
+package stqa.barancev.sandbox;
+
+/**
+ * Created by Natasha on 23.05.2016.
+ */
+public class Rectangle {
+    public double a;
+    public double b;
+    public Rectangle(double a,double b)
+    {
+        this.a=a;
+        this.b=b;
+    }
+}
---
+++ sandbox/src/main/java/stqa/barancev/sandbox/Square.java
@@ -0,0 +1,11 @@
+package stqa.barancev.sandbox;
+
+/**
+ * Created by Natasha on 23.05.2016.
+ */
+public class Square {
+    public double l;
+    public Square(double l){
+       this. l = l;
+    };
+}
