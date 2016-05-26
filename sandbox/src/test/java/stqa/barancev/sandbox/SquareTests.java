85a70b1448f3e43ef47051777f56fdcd76554308
Gennadii Miroshnychenko <genamister@gmail.com>
2016-05-27 00:05:14 +0300

    adressbok

---
+++ sandbox/src/test/java/stqa/barancev/sandbox/SquareTests.java
@@ -0,0 +1,15 @@
+package stqa.barancev.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Natasha on 26.05.2016.
+ */
+public class SquareTests {
+    @Test
+    public void testArea(){
+        Square s = new Square(5);
+        Assert.assertEquals(s.area(), 25.0);
+    }
+}
